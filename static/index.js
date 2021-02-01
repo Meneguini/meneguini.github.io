743a5228c1020464017dcc23ab5cdc1d3260e180
meneguini <mariana.meneguini@gmail.com>
2021-02-01 13:11:19 -0500

    prettier setup

--- static/index.js
+++ static/index.js
@@ -1,13 +1,12 @@
-document.addEventListener('DOMContentLoaded', () => {
-    document.querySelector('#menu-icon').addEventListener('click', menuClicked);
+document.addEventListener("DOMContentLoaded", () => {
+  document.querySelector("#menu-icon").addEventListener("click", menuClicked);
 });
 
 const menuClicked = () => {
-    console.log("menu cliked");
-    if(document.querySelector(".nav-pills").style.display == 'block') {
-        document.querySelector(".nav-pills").style.display = 'none';
-    }
-    else {
-        document.querySelector(".nav-pills").style.display = 'block';   
-    }
-}
+  console.log("menu clicked");
+  if (document.querySelector(".nav-pills").style.display == "block") {
+    document.querySelector(".nav-pills").style.display = "none";
+  } else {
+    document.querySelector(".nav-pills").style.display = "block";
+  }
+};
